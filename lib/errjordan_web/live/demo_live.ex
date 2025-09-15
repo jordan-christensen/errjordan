6805358827c968419af6ce9881a356584900f472
Jordan Christensen <jkchristensen36@gmail.com>
2025-09-15 11:53:52 -0700

    include colocated hooks

---
+++ lib/errjordan_web/live/demo_live.ex
@@ -0,0 +1,23 @@
+defmodule ErrjordanWeb.DemoLive do
+  use ErrjordanWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, :now, DateTime.utc_now())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <Layouts.app flash={@flash}>
+      <div id="demo-live" class="space-y-6">
+        <h1 class="text-2xl font-semibold">Demo LiveView</h1>
+        <p class="text-base-content/70">Now: {@now}</p>
+
+        <div id="hello-hook-target" phx-hook="HelloHook" class="p-4 rounded border border-base-300">
+          This box has phx-hook="HelloHook". Open the JS console to see it mount.
+        </div>
+      </div>
+    </Layouts.app>
+    """
+  end
+end
+
